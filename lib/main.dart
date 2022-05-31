01ec33786b324f2b6ed1e4f581da54bf69f27bfd
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-05-31 20:44:45 +0200

    Add question text and button texts

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,27 @@ class QuizApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('My App')),
-        body: const Text('My Quiz App'),
-      ), // Scaffold
-    ); // MaterialApp
+        home: Scaffold(
+            appBar: AppBar(title: const Text('My App')),
+            body: Column(children: const [
+              Text('The questions'),
+              RaisedButton(
+                child: Text('Answer1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer3'),
+                onPressed: null,
+              )
+            ]))); // MaterialApp
   } // build
 } // QuizApp
