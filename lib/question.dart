41a1c64c629e771684b882dda17bc5273b53771b
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-06-01 20:48:19 +0200

    quick fix

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-
   final String questionText;
-  Question(this.questionText, {Key? key}) : super(key: key)
+  Question(this.questionText, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
