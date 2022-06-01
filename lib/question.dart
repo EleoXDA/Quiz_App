2a720c1051ccaadcf367a55be635122464b7c856
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-06-01 20:54:32 +0200

    fix question text not aligning to center

--- lib/question.dart
+++ lib/question.dart
@@ -6,10 +6,12 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      questionText,
-      style: const TextStyle(fontSize: 28),
-      textAlign: TextAlign.center,
-    );
+    return Container(
+        width: double.infinity,
+        child: Text(
+          questionText,
+          style: const TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
